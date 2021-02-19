173febd80a3a12501ce8d9d4afde3832fad0e6bb
alexlee-dev <alex@alexlee.dev>
2021-02-18 23:16:47 -0800

    ✏️ ESLint Ignore /build and /dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
   ],
+  ignorePatterns: ["/build/**/*", "/dist/**/*"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
